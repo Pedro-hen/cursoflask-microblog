c1563b4f8ad9bd518b76986d2c7d659a10e748a2
Pedro Sousa <phenrique7090@gmail.com>
2024-04-21 11:28:05 -0300

    passagem de paramentro via rotas

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Pedro Henrique"
-    dados = {"profissao": "TI", "idade": 26}
+@app.route('/index', defaults={"nome":"User"})
+@app.route('/index/<nome>/<profissao>/<idade>')
+def index(nome, profissao, idade):
+    dados = {"profissao": profissao, "idade": idade}
     return render_template('index.html', nome=nome, dados=dados)
 
 
